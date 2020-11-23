f8e4e4950826446743bf30d4d3782b2b59e98fd1
Amine Hadj-Youcef <hadjyoucef.amine@gmail.com>
2020-11-23 09:31:43 +0100

    Clean the code

--- servier/src/main.py
+++ servier/src/main.py
@@ -1,20 +1,21 @@
-import os
 import argparse
+import os
 import time
+
 import joblib
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
 import servier.src.config as config
 import servier.src.feature_extractor as fe
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 def LoadAndProcessData(path=config.path_single):
 
-    print('[INFO] Load the data and extract features')
     isinstance(path, str)
+    print('[INFO] Load the data and extract features')
 
     # load the csv file
     df = pd.read_csv(path)
@@ -54,6 +55,7 @@ def SaveModelToDisk(model, path=config.MODEL_OUTPUT):
     """
     Save the pretrained machine learning model to disk
     """
+    isinstance(path, str)
     joblib_file = os.path.join(path)
     joblib.dump(model, joblib_file)
 
@@ -62,32 +64,12 @@ def LoadModelFromDisk(path):
     """
     Load a pretrained model from a (.bin) file
     """
+    isinstance(path, str)
+
     try:
         return joblib.load(path)
     except:
         raise ValueError('The file', path, 'can not be found')
-
-
-def create_folds_stratefied(path_dataset):
-    # Training data is in a CSV file called train.csv
-    df = pd.read_csv(path_dataset)
-
-    # we create a new column called kfold and fill it with -1
-    df["kfold"] = -1
-
-    # the next step is to randomize the rows of the data
-    df = df.sample(frac=1).reset_index(drop=True)
-    y = df.target.values
-
-    # initiate the kfold class from model_selection module
-    kf = model_selection.StratifiedKFold(n_splits=5)
-
-    # fill the new kfold column
-    for fold, (trn_, val_) in enumerate(kf.split(X=df, y=y)):
-        df.loc[val_, 'kfold'] = fold
-
-    # save the new csv with kfold column
-    df.to_csv(path_dataset[:-4]+'_folds.csv', index=False)
 
 
 def Train(data_path=config.MODEL_OUTPUT['single']):
